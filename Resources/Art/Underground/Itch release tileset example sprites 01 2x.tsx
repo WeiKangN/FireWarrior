49c9e1c0c1f8ae6b9ef6c3b304f222d67aceddc6
WeiKang <dkhuonghcm@outlook.com>
2018-11-08 19:34:59 +0700

    delete all

--- Resources/Art/Underground/Itch release tileset example sprites 01 2x.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="Itch release tileset example sprites 01 2x" tilewidth="8" tileheight="8" tilecount="6664" columns="68">
- <image source="Itch release tileset example sprites 01 2x.png" width="544" height="784"/>
-</tileset>
